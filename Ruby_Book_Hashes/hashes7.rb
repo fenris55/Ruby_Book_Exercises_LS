05d019eed3db290a6ddac708c3a94026fad2e6c7
Rebecca Biancofiore <rebeccabiancofiore79@gmail.com>
2022-07-30 20:42:54 +0000

    final Hashes chapter exercise

---
+++ Ruby_Book_Hashes/hashes7.rb
@@ -0,0 +1,12 @@
+#Exercise 7 Hashes chapter
+#explain the error message
+
+#NoMethodError: undefined method `keys' for Array
+
+=begin
+tried to use a hash method on an array and it can't work because an array doesnt have keys? 
+
+oh there are options
+gonna guess B - there is no method keys for an Array object
+checked Ruby docs - correct
+=end
